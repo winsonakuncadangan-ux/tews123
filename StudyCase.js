4823016440f0a25fdff5c3662c6b8afd2321ddba
winsonakuncadangan-ux <winsonakuncadangan@gmail.com>
2026-08-21 13:06:04 +0700

    Add switch case for grading system

---
+++ StudyCase.js
@@ -0,0 +1,17 @@
+let nilai = 100
+
+switch (true)
+{
+case (nilai >= 90):
+    console.log("Nilai anda A");
+    break;
+case (nilai >= 80):
+    console.log("Nilai anda B");
+    break;
+case (nilai >= 70):
+    console.log ("nilai anda C");
+    break;
+default:
+    console.log ("Anda mendapatkan nilai D");
+    break;
+}
